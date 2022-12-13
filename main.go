eccd2fcdda29755e96c4098e6d9dcc442df4e4be
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-13 17:01:48 +0000

    Add HTML files back to server

--- main.go
+++ main.go
@@ -7,11 +7,15 @@ import (
 )
 
 func sayHello(c *gin.Context) {
-	c.String(http.StatusOK, "Hello World!")
+	// c.String(http.StatusOK, "Hello World!")
+	c.HTML(http.StatusOK, "index.html", nil)
 }
 
 func main() {
 	r := gin.Default()
+	r.SetTrustedProxies([]string{"127.0.0.1"})
+	r.LoadHTMLGlob("static/*.html")
+
 	r.GET("/", sayHello)
 	r.Run(":8080")
 }
