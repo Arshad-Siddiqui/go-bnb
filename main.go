c05befde6d5dd990437d0570fa7c47f5579eed85
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-25 21:20:45 +0000

    Link a sign up page

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ func index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", nil)
 }
 
+func signup(c *gin.Context) {
+	c.HTML(http.StatusOK, "signup.html", nil)
+}
+
 func main() {
 	r := gin.Default()
 	r.SetTrustedProxies([]string{"127.0.0.1"})
@@ -18,5 +22,6 @@ func main() {
 	r.LoadHTMLGlob("static/*.html")
 
 	r.GET("/", index)
+	r.GET("/signup", signup)
 	r.Run(":8080")
 }
