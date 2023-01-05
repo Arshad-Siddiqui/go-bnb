3ad35054bb82a5cf559e9f102315494b3687777c
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-01-05 00:13:09 +0000

    Remove gin code and replace with hello world

--- main.go
+++ main.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
-func index(c *gin.Context) {
-	// c.String(http.StatusOK, "Hello World!")
-	c.HTML(http.StatusOK, "index.html", nil)
-}
-
-func signup(c *gin.Context) {
-	c.HTML(http.StatusOK, "signup.html", nil)
-}
-
 func main() {
-	r := gin.Default()
-	r.SetTrustedProxies([]string{"127.0.0.1"})
-	r.Static("/static", "./static/")
-	r.LoadHTMLGlob("static/*.html")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
 
-	r.GET("/", index)
-	r.GET("/signup", signup)
-	r.Run(":8080")
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
 }
