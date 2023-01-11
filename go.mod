86ceb8eeb3f70853ecb13d3e1415399af6ef2732
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-01-11 12:31:43 +0000

    Empty main.go

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/Arshad-Siddiqui/go-bnb
 
 go 1.19
-
-require github.com/joho/godotenv v1.4.0
--- main.go
+++ main.go
@@ -1,49 +1,5 @@
 package main
 
-import (
-	"html/template"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/joho/godotenv"
-)
-
-var staticPath = "./static/"
-var tmplPath = "./tmpl/"
-
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
-	http.HandleFunc("/", handler)
-	http.Handle(staticPath, http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
-	port := os.Getenv("PORT")
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		index(w, r)
-	default:
-		http.NotFound(w, r)
-	}
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	fileName := "index.html"
-	t, err := template.ParseFiles(tmplPath + fileName)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = t.ExecuteTemplate(w, fileName, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
 }
