a8d0682aebe7b40952119709776824b428583f85
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-01-11 15:02:50 +0000

    Redo file structure

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	topMux := http.NewServeMux()
+	topMux.Handle("/", http.FileServer(http.Dir("static/home")))
+	log.Fatal(http.ListenAndServe(":8080", topMux))
 }
