3461caed34f95080f17a91100f5fe0b316b31077
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-01-12 18:27:55 +0000

    Link signup page to main.go

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"log"
-	"net/http"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	topMux := http.NewServeMux()
-	topMux.Handle("/", http.FileServer(http.Dir("static/home")))
-	log.Fatal(http.ListenAndServe(":8080", topMux))
+	app := fiber.New(fiber.Config{
+		AppName: "GoBnB",
+	})
+
+	app.Static("/signup", "./static/signup", fiber.Static{
+		Index: "signup.html",
+	})
+	app.Static("/", "./static/home")
+	log.Fatal(app.Listen(":3000"))
 }
