b12dcf50de3449068280fd81012bd1a8b8cc6677
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-01-13 11:43:53 +0000

    Add templates

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"github.com/gofiber/template/html"
+
 	"os"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,14 +12,32 @@ import (
 )
 
 func main() {
+	engine := html.New("./views", ".html")
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 	app := fiber.New(fiber.Config{
-		AppName: "GoBnB",
+		AppName: "gobnb",
+		Views:   engine,
 	})
 
-	app.Static("/", "static")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "Home",
+		})
+	})
+
+	app.Get("/signup", func(c *fiber.Ctx) error {
+		return c.Render("signup", fiber.Map{
+			"Title": "Signup",
+		})
+	})
+
+	app.Get("/login", func(c *fiber.Ctx) error {
+		return c.SendString("Login test")
+	})
+
 	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
 }
