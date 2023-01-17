a7109f347dd8772092e4aff2b147828e287c7b73
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-01-17 14:42:23 +0000

    Add form tags

--- initializers/database.go
+++ initializers/database.go
@@ -11,7 +11,7 @@ var DB *gorm.DB
 
 func InitDB() {
 	// Example "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-	dsn := os.Getenv("DATABASE_URL")
+	dsn := os.Getenv("TEST_DATABASE_URL")
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
--- models/user.go
+++ models/user.go
@@ -6,8 +6,8 @@ import (
 
 type User struct {
 	gorm.Model
-	Email     string `json:"email"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Password  string `json:"password"`
+	Email     string `json:"email" form:"email" gorm:"unique;not null"`
+	FirstName string `json:"first_name" form:"first_name"`
+	LastName  string `json:"last_name" form:"last_name"`
+	Password  string `json:"password" form:"password" gorm:"not null"`
 }
