12d696b89fe8e2ea0a555c75c31e03e5381e4df4
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-01-18 10:11:43 +0000

    Use multiple env files

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -3,11 +3,18 @@ package initializers
 import (
 	"log"
 
+	"os"
+
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	if err := godotenv.Load(); err != nil {
+	env := os.Getenv("GOBNB_ENV")
+	if env == "" {
+		env = "development"
+	}
+
+	if err := godotenv.Load(".env." + env); err != nil {
 		log.Fatal("Error loading .env file")
 	}
 }
