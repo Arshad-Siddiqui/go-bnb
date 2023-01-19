461ac520a5fb9e54d93e8387001083fd62171efe
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-01-19 12:54:53 +0000

    Move routes to router.go

--- main.go
+++ main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"log"
 
-	"github.com/Arshad-Siddiqui/go-bnb/controllers"
 	"github.com/Arshad-Siddiqui/go-bnb/initializers"
 	"github.com/Arshad-Siddiqui/go-bnb/router"
 
 	"os"
-
-	"github.com/gofiber/fiber/v2"
 )
 
 func init() {
@@ -20,41 +17,5 @@ func init() {
 func main() {
 	app := router.New()
 
-	app.Static("/", "./assets")
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", fiber.Map{})
-	})
-
-	app.Get("/signup", func(c *fiber.Ctx) error {
-		return c.Render("signup", fiber.Map{})
-	})
-
-	app.Post("/signup", func(c *fiber.Ctx) error {
-		return controllers.UserCreate(c)
-	})
-
-	app.Get("/login", func(c *fiber.Ctx) error {
-		return c.Render("login", fiber.Map{})
-	})
-
-	app.Post("/login", func(c *fiber.Ctx) error {
-		return controllers.UserAuthenticate(c)
-	})
-
-	app.Get("/users", func(c *fiber.Ctx) error {
-		return controllers.UserIndex(c)
-	})
-
-	app.Post("/listings", func(c *fiber.Ctx) error {
-		return controllers.ListingCreate(c)
-	})
-
-	app.Get("/listings", func(c *fiber.Ctx) error {
-		return controllers.ListingIndex(c)
-	})
-
-	app.Delete("/listings/:id", func(c *fiber.Ctx) error {
-		return controllers.ListingDelete(c)
-	})
 	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
 }
--- router/router.go
+++ router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/Arshad-Siddiqui/go-bnb/controllers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/template/html"
@@ -14,5 +15,41 @@ func New() *fiber.App {
 		Views:   engine,
 	})
 	app.Use(logger.New())
+	app.Static("/", "./assets")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{})
+	})
+
+	app.Get("/signup", func(c *fiber.Ctx) error {
+		return c.Render("signup", fiber.Map{})
+	})
+
+	app.Post("/signup", func(c *fiber.Ctx) error {
+		return controllers.UserCreate(c)
+	})
+
+	app.Get("/login", func(c *fiber.Ctx) error {
+		return c.Render("login", fiber.Map{})
+	})
+
+	app.Post("/login", func(c *fiber.Ctx) error {
+		return controllers.UserAuthenticate(c)
+	})
+
+	app.Get("/users", func(c *fiber.Ctx) error {
+		return controllers.UserIndex(c)
+	})
+
+	app.Post("/listings", func(c *fiber.Ctx) error {
+		return controllers.ListingCreate(c)
+	})
+
+	app.Get("/listings", func(c *fiber.Ctx) error {
+		return controllers.ListingIndex(c)
+	})
+
+	app.Delete("/listings/:id", func(c *fiber.Ctx) error {
+		return controllers.ListingDelete(c)
+	})
 	return app
 }
