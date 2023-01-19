01e5190e57743c3aa34994bfc1585bc54a68f6c7
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-01-19 13:27:52 +0000

    Group user routes

--- router/router.go
+++ router/router.go
@@ -20,25 +20,7 @@ func New() *fiber.App {
 		return c.Render("index", fiber.Map{})
 	})
 
-	app.Get("/signup", func(c *fiber.Ctx) error {
-		return c.Render("signup", fiber.Map{})
-	})
-
-	app.Post("/signup", func(c *fiber.Ctx) error {
-		return controllers.UserCreate(c)
-	})
-
-	app.Get("/login", func(c *fiber.Ctx) error {
-		return c.Render("login", fiber.Map{})
-	})
-
-	app.Post("/login", func(c *fiber.Ctx) error {
-		return controllers.UserAuthenticate(c)
-	})
-
-	app.Get("/users", func(c *fiber.Ctx) error {
-		return controllers.UserIndex(c)
-	})
+	addUserRoutes(app)
 
 	app.Post("/listings", func(c *fiber.Ctx) error {
 		return controllers.ListingCreate(c)
---
+++ router/user.go
@@ -0,0 +1,31 @@
+package router
+
+import (
+	"github.com/Arshad-Siddiqui/go-bnb/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func addUserRoutes(app *fiber.App) {
+	user := app.Group("user")
+
+	user.Get("/signup", func(c *fiber.Ctx) error {
+		return c.Render("signup", fiber.Map{})
+	})
+
+	user.Post("/signup", func(c *fiber.Ctx) error {
+		return controllers.UserCreate(c)
+	})
+
+	user.Get("/login", func(c *fiber.Ctx) error {
+		return c.Render("login", fiber.Map{})
+	})
+
+	user.Post("/login", func(c *fiber.Ctx) error {
+		return controllers.UserAuthenticate(c)
+	})
+
+	user.Get("/users", func(c *fiber.Ctx) error {
+		return controllers.UserIndex(c)
+	})
+
+}
